4fb7f55dd079520172e0e929a63ac44792b8bde7
ju1 <ju1@upleat.com>
2022-05-25 00:05:17 +0900

    박보영 갤러리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import data from './data.js';
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 	
@@ -10,24 +12,33 @@ function App() {
 	let [modal, setModal] = useState(false);
 	let [index, setIndex] = useState(0);
 
+	let [fashion] = useState(data);
+
 	return (
 		<div className="App">
+
+{/* git init
+git add .
+git status
+git config --global user.email "ju1@upleat.com" (첫번째 후 생략)
+git config --global user.name "ju1" (첫번째 후 생략)
+git commit -m "first commit"
+git remote add origin https://github.com/upleatPKO/johnny.git (첫번째 후 생략)
+git branch -M main (첫번째 후 생략)
+git push -u origin main
+git branch issue1 
+git clone https://github.com/upleatPKO/ju1.git */}
+		
+
 			<div className="black-nav">
 				<h4>ReactBlog</h4>
 			</div>
 
-			{/* <div className="list">
-				<h4>{ 글제목[0] }</h4>
-				<p>2월 17일 발행</p>
+			<div className="Nav_bar">
+				<h1>Fashion</h1>
+				<a href="Home">Home</a>
+				<a href="Cart">Cart</a>
 			</div>
-			<div className="list">
-				<h4>{ 글제목[1] }</h4>
-				<p>2월 17일 발행</p>
-			</div>
-			<div className="list">
-				<h4>{ 글제목[2] }</h4>
-				<p>2월 17일 발행</p>
-			</div> */}
 
 			{
 				글제목.map(function(v,i){
@@ -56,7 +67,59 @@ function App() {
 			{
 				(modal == true) ? <Modal setModal={setModal} setIndex={setIndex} index={index} 글제목={글제목} 날짜={날짜} 상세내용={상세내용} modal={modal} /> : null
 			}
-			
+
+
+			<div className="align_c">
+				<img src={process.env.PUBLIC_URL + '/pby.jpg'} alt="" />
+			</div>
+
+			<div className="fashions">
+				{/* <div className="fashions_item">
+					<img src={process.env.PUBLIC_URL + '/fashion1.jpg'} alt="" />
+					<h4>{fashion[0].title}</h4>
+					<p>{fashion[0].content}</p>
+					<p>{fashion[1].price} 원</p>
+				</div>
+				<div className="fashions_item">
+					<img src={process.env.PUBLIC_URL + '/fashion2.jpg'} alt="" />
+					<h4>{fashion[1].title}</h4>
+					<p>{fashion[1].content}</p>
+					<p>{fashion[1].price} 원</p>
+				</div>
+				<div className="fashions_item">
+					<img src={process.env.PUBLIC_URL + '/fashion3.jpg'} alt="" />
+					<h4>{fashion[2].title}</h4>
+					<p>{fashion[2].content}</p>
+					<p>{fashion[2].price} 원</p>
+				</div> */}
+				
+				{
+					fashion.map(function(v,i){
+						return (
+							<div className="fashions_item">
+								<img src={process.env.PUBLIC_URL + '/fashion'+ (i+1) +'.jpg'} alt="" />
+								<h4>{fashion[i].title}</h4>
+								<p>{fashion[i].content}</p>
+								<p>{fashion[i].price} 원</p>
+							</div>
+						)
+					})
+				}
+			</div>
+
+			{/* <div className="list">
+				<h4>{ 글제목[0] }</h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{ 글제목[1] }</h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{ 글제목[2] }</h4>
+				<p>2월 17일 발행</p>
+			</div> */}
+		
 
 		</div>
 	);
